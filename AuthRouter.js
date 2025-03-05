590785ad4aaa6ecfea43bb73e7ed8abfa71855f9
AyusAgarwal <99806027+AyusAgarwal@users.noreply.github.com>
2025-03-05 17:11:53 +0530

    Update AuthRouter.js

--- AuthRouter.js
+++ AuthRouter.js
@@ -1,9 +1,11 @@
 const { signup, login } = require('../Controllers/AuthController');
 const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
+const passwordreset=require('../Controllers/PasswordReset')
 
 const router = require('express').Router();
 
 router.post('/login', loginValidation, login);
 router.post('/signup', signupValidation, signup);
-
+router.post('/requespassword',passwordreset);
+router.post('/resetPassword',resetPassword);
 module.exports = router;
